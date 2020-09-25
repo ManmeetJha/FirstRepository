b842a866bcd6e1f6ffe8ac48033b679a32a4765f
Navneet <navneetjh5@gmail.com>
2020-09-25 21:58:36 +0530

    UC1 updated

--- JavaPractice/src/main/java/com/javapractice/day4/SnakeAndLadder.java
+++ JavaPractice/src/main/java/com/javapractice/day4/SnakeAndLadder.java
@@ -2,34 +2,37 @@ package com.javapractice.day4;
 
 import java.util.Random;
 
-public class SnakeAndLadder{
+public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		int position = 0;
 		System.out.println("SNAKE AND LADDER GAME");
 		System.out.println("Welcome");
 
-		int dice = (int) Math.floor(Math.random() * 5) + 1;
-		System.out.println(dice);
+		Random random = new Random();
+		int count = 0;
+		do {
+			count++;
+			int dice = random.nextInt(6) + 1;
+			Random r = new Random();
+			String[] opr = { "No_Play", "Ladder", "Snake" };
+			int r_opr = r.nextInt(opr.length);
 
-		System.out.println("Random Operation");
-		Random r = new Random();
-		String[] opr = { "No_Play", "Ladder", "Snake" };
-		int r_opr = r.nextInt(opr.length);
-		System.out.println(opr[r_opr]);
+			switch (opr[r_opr]) {
+			case "No_Play":
+				break;
+			case "Ladder":
+				if (position + dice <= 100)
+					position += dice;
+				break;
+			case "Snake":
+				if (position - dice < 0)
+					position = 0;
+				break;
+			}
+		} while (position != 100);
 
-		switch (opr[r_opr]) {
-		case "No_Play":
-			break;
-		case "Ladder":
-			position += dice;
-			break;
-		case "Snake":
-			position -= dice;
-			break;
-		}
-		System.out.println("New position is:"+position);
-
+		System.out.println("count: " + count);
 	}
 
 }
