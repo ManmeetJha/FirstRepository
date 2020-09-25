6739f2e9ef7326b15ac9506a6e3ae4a82197ae2d
Navneet <navneetjh5@gmail.com>
2020-09-25 20:53:02 +0530

    UC3 code updated

--- JavaPractice/src/main/java/com/javapractice/day5/lineComparision.java
+++ JavaPractice/src/main/java/com/javapractice/day5/lineComparision.java
@@ -1,9 +1,35 @@
 package com.javapractice.day5;
 
+import java.util.Scanner;
+
 public class lineComparision {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line comparision computation program");
+		Scanner cor = new Scanner(System.in);
+		System.out.println("Enter Cordinates of line 1:");
+		Double length1 = length(cor);
+		System.out.println("Enter Cordinates of line 2:");
+		Double length2 = length(cor);
+		if(length1.equals(length2)) {
+			System.out.println("Both lines are equal");
+		}else {
+			System.out.println("Both lines are not equal");
+		}
+
 	}
 
+	public static double length(Scanner cor) {
+		System.out.println("Enter x1:");
+		int x1 = cor.nextInt();
+		System.out.println("Enter y1:");
+		int y1 = cor.nextInt();
+		System.out.println("Enter x2:");
+		int x2 = cor.nextInt();
+		System.out.println("Enter y2:");
+		int y2 = cor.nextInt();
+
+		double len = Math.sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2);
+		return len;
+	}
 }
