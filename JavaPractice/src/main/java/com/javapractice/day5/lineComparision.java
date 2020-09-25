a8e03f4e0e62d9a8dc684f955a8da71373ebc381
Navneet <navneetjh5@gmail.com>
2020-09-25 20:29:02 +0530

    UC1 created

---
+++ JavaPractice/src/main/java/com/javapractice/day5/lineComparision.java
@@ -0,0 +1,9 @@
+package com.javapractice.day5;
+
+public class lineComparision {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line comparision computation program");
+	}
+
+}
